a95aa898e206d2f0d19016d65d1e9a8c44c8a0cf
pointnet <aurelien.verla@gmail.com>
2016-07-14 15:37:18 +0200

    rework

--- generator.js
+++ generator.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var path = require('path');
-var templates = path.resolve.bind(path, __dirname, 'templates');
 var isValid = require('is-valid-app');
 
 module.exports = function(app) {
@@ -11,42 +9,269 @@ module.exports = function(app) {
    * Plugins
    */
 
-  app.use(require('generate-collections'));
   app.use(require('generate-defaults'));
 
   /**
-   * Generate a `LICENSE` file in the current working directory.
-   * _(You will be prompted for the file name to use)_.
+   * Generate a GNU Affero General Public License v3.0 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:agpl-3.0
+   * $ gen license:agpl-3.0 --dest ./docs
+   * $ gen dest license:agpl-3.0
+   * ```
+   * @name license:agpl-3.0
+   * @api public
+   */
+
+  app.task('agpl-3.0', { silent: true }, function () {
+    return file(app, 'agpl-3.0');
+  });
+
+  /**
+   * Generate a Apache License 2.0 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:apache-2.0
+   * $ gen license:apache-2.0 --dest ./docs
+   * $ gen dest license:apache-2.0
+   * ```
+   * @name license:apache-2.0
+   * @api public
+   */
+
+  app.task('apache-2.0', { silent: true }, function () {
+    return file(app, 'apache-2.0');
+  });
+
+  /**
+   * Generate a Artistic License 2.0 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:artistic-2.0
+   * $ gen license:artistic-2.0 --dest ./docs
+   * $ gen dest license:artistic-2.0
+   * ```
+   * @name license:artistic-2.0
+   * @api public
+   */
+
+  app.task('artistic-2.0', { silent: true }, function () {
+    return file(app, 'artistic-2.0');
+  });
+
+  /**
+   * Generate a BSD 2-clause "Simplified" License `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:bsd-2-clause
+   * $ gen license:bsd-2-clause --dest ./docs
+   * $ gen dest license:bsd-2-clause
+   * ```
+   * @name license:bsd-2-clause
+   * @api public
+   */
+
+  app.task('bsd-2-clause', { silent: true }, function () {
+    return file(app, 'bsd-2-clause');
+  });
+
+  /**
+   * Generate a BSD 3-clause "New" or "Revised" License `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:bsd-3-clause
+   * $ gen license:bsd-3-clause --dest ./docs
+   * $ gen dest license:bsd-3-clause
+   * ```
+   * @name license:bsd-3-clause
+   * @api public
+   */
+
+  app.task('bsd-3-clause', { silent: true }, function () {
+    return file(app, 'bsd-3-clause');
+  });
+
+  /**
+   * Generate a Creative Commons Zero v1.0 Universal `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:cc0-1.0
+   * $ gen license:cc0-1.0 --dest ./docs
+   * $ gen dest license:cc0-1.0
+   * ```
+   * @name license:cc0-1.0
+   * @api public
+   */
+
+  app.task('cc0-1.0', { silent: true }, function () {
+    return file(app, 'cc0-1.0');
+  });
+
+  /**
+   * Generate a Eclipse Public License 1.0 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:epl-1.0
+   * $ gen license:epl-1.0 --dest ./docs
+   * $ gen dest license:epl-1.0
+   * ```
+   * @name license:epl-1.0
+   * @api public
+   */
+
+  app.task('epl-1.0', { silent: true }, function () {
+    return file(app, 'epl-1.0');
+  });
+
+  /**
+   * Generate a GNU General Public License v2.0 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:gpl-2.0
+   * $ gen license:gpl-2.0 --dest ./docs
+   * $ gen dest license:gpl-2.0
+   * ```
+   * @name license:gpl-2.0
+   * @api public
+   */
+
+  app.task('gpl-2.0', { silent: true }, function () {
+    return file(app, 'gpl-2.0');
+  });
+
+  /**
+   * Generate a GNU General Public License v3.0 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:gpl-3.0
+   * $ gen license:gpl-3.0 --dest ./docs
+   * $ gen dest license:gpl-3.0
+   * ```
+   * @name license:gpl-3.0
+   * @api public
+   */
+
+  app.task('gpl-3.0', { silent: true }, function () {
+    return file(app, 'gpl-3.0');
+  });
+
+  /**
+   * Generate a ISC License `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:isc
+   * $ gen license:isc --dest ./docs
+   * $ gen dest license:isc
+   * ```
+   * @name license:isc
+   * @api public
+   */
+
+  app.task('isc', { silent: true }, function () {
+    return file(app, 'isc');
+  });
+
+  /**
+   * Generate a GNU Lesser General Public License v2.1 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:lgpl-2.1
+   * $ gen license:lgpl-2.1 --dest ./docs
+   * $ gen dest license:lgpl-2.1
+   * ```
+   * @name license:lgpl-2.1
+   * @api public
+   */
+
+  app.task('lgpl-2.1', { silent: true }, function () {
+    return file(app, 'lgpl-2.1');
+  });
+
+  /**
+   * Generate a GNU Lesser General Public License v3.0 `LICENSE.lesser`
+   * file in the current working directory.
+   *
+   * This will also generate a GNU General Public License v3.0 `LICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:lgpl-3.0
+   * $ gen license:lgpl-3.0 --dest ./docs
+   * $ gen dest license:lgpl-3.0
+   * ```
+   * @name license:lgpl-3.0
+   * @api public
+   */
+
+  app.task('lgpl-3.0', ['gpl-3.0'], function () {
+    return file(app, 'lgpl-3.0');
+  });
+
+  /**
+   * Generate a MIT License `LICENSE`
+   * file in the current working directory.
    *
    * ```sh
    * $ gen license:mit
-   * # or
-   * $ gen license
+   * $ gen license:mit --dest ./docs
+   * $ gen dest license:mit
    * ```
    * @name license:mit
    * @api public
    */
 
-  app.task('license', ['mit']);
-  app.task('mit', { silent: true }, function() {
+  app.task('mit', { silent: true }, function () {
     return file(app, 'mit');
   });
 
   /**
-   * Generate a Creative Commons `LICENSE-CC` file in the current working directory.
-   * _(You will be prompted for the file name to use)_.
+   * Generate a Mozilla Public License 2.0 `LICENSE`
+   * file in the current working directory.
    *
    * ```sh
-   * $ gen license:cc
+   * $ gen license:mpl-2.0
+   * $ gen license:mpl-2.0 --dest ./docs
+   * $ gen dest license:mpl-2.0
    * ```
-   * @name license:cc
+   * @name license:mpl-2.0
    * @api public
    */
 
-  app.task('creative-commons', ['cc']);
-  app.task('cc', { silent: true }, function() {
-    return file(app, 'cc');
+  app.task('mpl-2.0', { silent: true }, function () {
+    return file(app, 'mpl-2.0');
   });
+
+  /**
+   * Generate a The Unlicense `UNLICENSE`
+   * file in the current working directory.
+   *
+   * ```sh
+   * $ gen license:unlicense
+   * $ gen license:unlicense --dest ./docs
+   * $ gen dest license:unlicense
+   * ```
+   * @name license:unlicense
+   * @api public
+   */
+
+  app.task('unlicense', { silent: true }, function () {
+    return file(app, 'unlicense');
+  });
+
+
+  // TODO: license:choose
+  // app.task('license', ['mit']);
 
   /**
    * Alias for the [license](#license) task.
@@ -57,7 +282,6 @@ module.exports = function(app) {
    * @name license
    * @api public
    */
-
   app.task('default', { silent: true }, ['license']);
 };
 
@@ -66,9 +290,8 @@ module.exports = function(app) {
  */
 
 function file(app, name) {
-  var dest = app.option('dest') || app.cwd;
-  return app.src(templates(`${name}.tmpl`))
+  return app.src(`templates/${name}.tmpl`, { cwd: __dirname })
     .pipe(app.renderFile('*'))
-    .pipe(app.conflicts(dest))
-    .pipe(app.dest(dest));
+    .pipe(app.conflicts(app.cwd))
+    .pipe(app.dest(app.cwd));
 }
